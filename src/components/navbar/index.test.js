1dca292dec3335481762704fa3d88941fcd2352e
Taras Viyatyk <tviyatyk@gmail.com>
2020-06-13 04:22:55 +0300

    Update index.test.js

--- src/components/navbar/index.test.js
+++ src/components/navbar/index.test.js
@@ -7,13 +7,11 @@ import NavBar from "./index";
 
 let container = null;
 beforeEach(() => {
-  // подготавливаем DOM-элемент, куда будем рендерить
   container = document.createElement("div");
   document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // подчищаем после завершения
   unmountComponentAtNode(container);
   container.remove();
   container = null;
